4e77ef234646eee4e31f639a09d66903adba5566
sushantjoshi800 <79572922+sjoshihypen@users.noreply.github.com>
2025-03-17 12:37:11 +0530

    Update Facebook Sentiments.py

--- pages/Facebook Sentiments.py
+++ pages/Facebook Sentiments.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Set Streamlit page configuration
-st.set_page_config(page_title="BHEL: Social Sentiment Analysis", layout="wide")
+st.set_page_config(page_title="BHEL: Facebook Sentiment Analysis", layout="wide")
 
 # ============================ INLINE IMAGE ENCODING ============================ #
 def get_image_base64(image_path):
